f024e6056ab057960b3f019ffe3172eeb0cf7e2d
bunlong22 <suonbunlong93@gmail.com>
2023-06-13 22:29:56 +0700

    comments the http and url

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const http = require("http");
-const url = require("url");
+const http = require("http"); //require the http server
+const url = require("url");   //require the url
 
 let books = [];
 
